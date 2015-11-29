c445c07d151da16934dc3e485c81bf47179b76b2
soleiana <anna2111@gmai.com>
2015-11-29 16:28:52 +0200

    @TransactionConfiguration replaced

--- src/test/java/com/conferenceplanner/SpringContextTest.java
+++ src/test/java/com/conferenceplanner/SpringContextTest.java
@@ -7,13 +7,18 @@ import com.conferenceplanner.config.RestConfig;
 import com.conferenceplanner.config.DatasourceConfig;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.*;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 
+import javax.transaction.Transactional;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {RestConfig.class, DatasourceConfig.class})
-@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
+@Rollback(false)
+@org.springframework.transaction.annotation.Transactional(transactionManager = "transactionManager")
+//@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
 @Ignore
 public class SpringContextTest
 {}
