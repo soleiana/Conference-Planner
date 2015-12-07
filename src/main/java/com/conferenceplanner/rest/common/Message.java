216ef2e5230f71bd6215818f8583ca97c9f764c0
soleiana <anna2111@gmai.com>
2015-12-07 17:20:10 +0200

    cleanup

--- src/main/java/com/conferenceplanner/rest/common/Message.java
+++
@@ -1,9 +0,0 @@
-package com.conferenceplanner.rest.common;
-
-/**
- * Created by Ann on 17/09/14.
- */
-public class Message
-{
-    public final static String INVALID_INPUT_FORMAT = "Invalid input format";
-}
