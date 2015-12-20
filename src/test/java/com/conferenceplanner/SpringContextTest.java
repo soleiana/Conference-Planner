fab1b084a2190b2380720924d66568f080ec9193
soleiana <anna2111@gmai.com>
2015-12-20 15:06:20 +0200

    minor change

--- src/test/java/com/conferenceplanner/SpringContextTest.java
+++ src/test/java/com/conferenceplanner/SpringContextTest.java
@@ -8,12 +8,13 @@ import org.junit.runner.RunWith;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.*;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {RestConfig.class, DatasourceConfig.class})
 @Rollback(false)
-@org.springframework.transaction.annotation.Transactional(transactionManager = "transactionManager")
+@Transactional(transactionManager = "transactionManager")
 @Ignore
 public class SpringContextTest
 {}
