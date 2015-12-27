b2eaf29d09d640da98bc3fd3fe32800bf3d1eba6
soleiana <anna2111@gmai.com>
2015-12-27 17:07:36 +0200

    restassured test templates added

---
+++ src/test/java/com/conferenceplanner/restassured/CancelConferenceIntegrationTest.java
@@ -0,0 +1,21 @@
+package com.conferenceplanner.restassured;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class CancelConferenceIntegrationTest {
+
+    @Before
+    public void setUp() {}
+
+
+    @Test
+    public void test_cancelConference() {
+
+    }
+}
---
+++ src/test/java/com/conferenceplanner/restassured/CreateConferenceIntegrationTest.java
@@ -0,0 +1,22 @@
+package com.conferenceplanner.restassured;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class CreateConferenceIntegrationTest {
+
+    @Before
+    public void setUp() {}
+
+
+    @Test
+    public void test_createConference() {
+
+    }
+}
