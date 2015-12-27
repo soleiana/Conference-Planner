d9dcf09de2a12ff024f36af9fff96e0066a5fa1b
soleiana <anna2111@gmai.com>
2015-12-27 21:50:34 +0200

    create conference room restassured test

--- src/test/java/com/conferenceplanner/restassured/CreateConferenceRoomIntegrationTest.java
+++ src/test/java/com/conferenceplanner/restassured/CreateConferenceRoomIntegrationTest.java
@@ -1,21 +1,32 @@
 package com.conferenceplanner.restassured;
 
+import com.conferenceplanner.rest.domain.ConferenceRoom;
+import com.conferenceplanner.rest.fixtures.ConferenceRoomFixture;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+
 import static com.jayway.restassured.RestAssured.*;
-import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 public class CreateConferenceRoomIntegrationTest {
 
     @Before
-    public void setUp() {}
-
+    public void setUp() {
+        delete("/conference-planner/test-util/clean-database");
+    }
 
     @Test
     public void test_createConferenceRoom() {
-
+        ConferenceRoom conferenceRoom = ConferenceRoomFixture.createConferenceRoom();
+        given().
+                contentType("application/json").
+                body(conferenceRoom).
+        when().
+                post("/conference-planner/conference-rooms").
+                then().
+                assertThat().
+                body(equalTo("Conference room created.")).
+                assertThat().statusCode(201);
     }
 }
